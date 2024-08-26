31251f2365658b1799485133b1a0ea4a2cd440c9
abhishekpm <pabhishekm@gmail.com>
2024-08-26 21:11:56 +0530

    spring data JPA (ManyToOne, OneToMany)

--- src/main/java/com/example/Employee/controller/EmployeeController.java
+++ src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.example.Employee.controller;
 import com.example.Employee.model.Employee;
 import com.example.Employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
